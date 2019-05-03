cdef0edea21c142991c1012d73c07c009f774cd3
Rohit Kaundal <kaundal.rohit@gmail.com>
2019-05-03 17:08:48 +0530

    Bare bones

---
+++ Main.py
@@ -0,0 +1,11 @@
+# Author : Rohit Kaundal - Cyber Expert, Chandigarh Police
+# Snippets for hacking , ref: Grey Hat Python
+
+
+def main():
+	print("Grey Hat")
+
+
+if __name__ == "__main__":
+	main()
+
